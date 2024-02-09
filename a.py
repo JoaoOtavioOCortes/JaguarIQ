2ea8486870ce74bb953ead719f602d2100bbfff9
João Otávio <joaootavioocortes@gmail.com>
2024-02-08 22:43:22 -0300

    Tela iniciada e prototipos de botoes.

---
+++ a.py
@@ -0,0 +1,10 @@
+class b():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+    def draw(self, x):
+        #draw button on the screen
+        x.blit(self.image,(self.rect.x, self.rect.y))
+
